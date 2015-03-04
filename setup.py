9d730809d851451106867edce7d80f4438e5d7f5
Lars Kellogg-Stedman <lars@redhat.com>
2015-03-04 17:21:08 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    author       = 'Lars Kellogg-Stedman',
+    author_email = 'lars@oddbit.com',
+    url          = 'http://github.com/larsks/netns',
+    description  = 'Wrapper for the Linux setns() system call',
+    name         = 'netns',
+    py_modules   = ['netns'],
+    version      = 1,
+)
+
