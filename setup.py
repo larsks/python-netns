d047e03d5d9a93e24413e0fc971c3e22a75bd2a2
Lars Kellogg-Stedman <lars@redhat.com>
2015-03-04 20:33:05 -0500

    fixed url in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 setuptools.setup(
     author       = 'Lars Kellogg-Stedman',
     author_email = 'lars@oddbit.com',
-    url          = 'http://github.com/larsks/netns',
+    url          = 'http://github.com/larsks/python-netns',
     description  = 'Wrapper for the Linux setns() system call',
     name         = 'netns',
     py_modules   = ['netns'],
