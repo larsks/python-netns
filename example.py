09014c6d211b3465e1846834b5f88cb63f3faa2f
Lars Kellogg-Stedman <lars@oddbit.com>
2018-06-03 15:09:50 -0400

    add some comments to example setup

--- example.py
+++ example.py
@@ -11,14 +11,29 @@ import subprocess
 import time
 
 setup = [
+    # create a network namespace named "sandbox"
     'ip netns add sandbox',
+
+    # create a pair of veth interfaces named 
+    # "inside" and "outside"
     'ip link add outside type veth peer name inside',
+
+    # Assign the "inside" interface to the network namespace
     'ip link set netns sandbox inside',
+
+    # Assign an address to the "outside" interface
     'ip addr add 172.16.16.1/24 dev outside',
     'ip link set outside up',
+
+    # Assign an address to the "inside" interface
     'ip netns exec sandbox ip addr add 172.16.16.2/24 dev inside',
     'ip netns exec sandbox ip link set inside up',
+
+    # Set up a default route for the network namespace
     'ip netns exec sandbox ip route add default via 172.16.16.1',
+
+    # Arrange to masquerade outbound packets from the network
+    # namespace.
     'iptables -t nat -A POSTROUTING -s 172.16.16.0/24 -j MASQUERADE',
 ]
 
