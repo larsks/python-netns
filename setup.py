189536ddd78261a72b5bbcd2a7a50353b8b0d934
Aaron Edwards <aaedwards@paloaltonetworks.com>
2023-11-03 19:31:57 -0700

    Added unshare/bind_mount functions to replicate `ip netns exec` functionality in netns lib.
    Also made other refactors that are mostly cosmetic (module dir, more setup.py info, etc.)

--- setup.py
+++ setup.py
@@ -2,12 +2,30 @@
 
 import setuptools
 
+with open('README.md') as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     author='Lars Kellogg-Stedman',
     author_email='lars@oddbit.com',
     url='http://github.com/larsks/python-netns',
     description='Wrapper for the Linux setns() system call',
+    long_description=long_description,
     name='netns',
     license='GPLv3',
     py_modules=['netns'],
-    version='1.0')
+    version='1.1',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: System :: Networking",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
