e02e84aa749b2c6db3b3efd845d83eb08b773971
Lars Kellogg-Stedman <lars@oddbit.com>
2024-04-19 12:12:08 -0400

    Reformat with Black formatter

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 # This script sets up a network namespace, veth interfaces, routing,
-# and iptables rules permitting outbound access, and then opens a 
+# and iptables rules permitting outbound access, and then opens a
 # socket inside the namespace and makes a web request.
 #
 # The script must be run as root.  E.g:
@@ -8,46 +8,39 @@
 
 import netns
 import subprocess
-import time
 
 setup = [
     # create a network namespace named "sandbox"
-    'ip netns add sandbox',
-
-    # create a pair of veth interfaces named 
+    "ip netns add sandbox",
+    # create a pair of veth interfaces named
     # "inside" and "outside"
-    'ip link add outside type veth peer name inside',
-
+    "ip link add outside type veth peer name inside",
     # Assign the "inside" interface to the network namespace
-    'ip link set netns sandbox inside',
-
+    "ip link set netns sandbox inside",
     # Assign an address to the "outside" interface
-    'ip addr add 172.16.16.1/24 dev outside',
-    'ip link set outside up',
-
+    "ip addr add 172.16.16.1/24 dev outside",
+    "ip link set outside up",
     # Assign an address to the "inside" interface
-    'ip netns exec sandbox ip addr add 172.16.16.2/24 dev inside',
-    'ip netns exec sandbox ip link set inside up',
-
+    "ip netns exec sandbox ip addr add 172.16.16.2/24 dev inside",
+    "ip netns exec sandbox ip link set inside up",
     # Set up a default route for the network namespace
-    'ip netns exec sandbox ip route add default via 172.16.16.1',
-
+    "ip netns exec sandbox ip route add default via 172.16.16.1",
     # Arrange to masquerade outbound packets from the network
     # namespace.
-    'iptables -t nat -A POSTROUTING -s 172.16.16.0/24 -j MASQUERADE',
+    "iptables -t nat -A POSTROUTING -s 172.16.16.0/24 -j MASQUERADE",
 ]
 
 teardown = [
-    'ip link del outside',
-    'ip netns del sandbox',
-    'iptables -t nat -D POSTROUTING -s 172.16.16.0/24 -j MASQUERADE',
+    "ip link del outside",
+    "ip netns del sandbox",
+    "iptables -t nat -D POSTROUTING -s 172.16.16.0/24 -j MASQUERADE",
 ]
 
 
 def run_steps(steps, ignore_errors=False):
     for step in steps:
         try:
-            print('+ {}'.format(step))
+            print("+ {}".format(step))
             subprocess.check_call(step, shell=True)
         except subprocess.CalledProcessError:
             if ignore_errors:
@@ -55,14 +48,15 @@ def run_steps(steps, ignore_errors=False):
             else:
                 raise
 
+
 try:
     run_steps(setup)
 
-    s = netns.socket(netns.get_ns_path(nsname='sandbox'))
-    s.connect(('loripsum.net', 80))
-    s.send('GET /api/3/short HTTP/1.1\r\n')
-    s.send('Host: loripsum.net\r\n')
-    s.send('\r\n')
+    s = netns.socket(netns.get_ns_path(nsname="sandbox"))
+    s.connect(("loripsum.net", 80))
+    s.send("GET /api/3/short HTTP/1.1\r\n")
+    s.send("Host: loripsum.net\r\n")
+    s.send("\r\n")
     d = s.recv(1024)
     print(d)
 finally:
--- netns.py
+++ netns.py
@@ -15,74 +15,73 @@ def errcheck(ret, func, args):
         e = get_errno()
         raise OSError(e, os.strerror(e))
 
-libc = CDLL('libc.so.6', use_errno=True)
+
+libc = CDLL("libc.so.6", use_errno=True)
 libc.setns.errcheck = errcheck
 
 
 def setns(fd, nstype):
-    '''Change the network namespace of the calling thread.
+    """Change the network namespace of the calling thread.
 
     Given a file descriptor referring to a namespace, reassociate the
     calling thread with that namespace.  The fd argument may be either a
     numeric file  descriptor or a Python object with a fileno() method.
-    '''
+    """
 
-    if hasattr(fd, 'fileno'):
+    if hasattr(fd, "fileno"):
         fd = fd.fileno()
 
     return libc.setns(fd, nstype)
 
 
 def socket(nspath, *args):
-    '''Return a socket from a network namespace.
+    """Return a socket from a network namespace.
 
     This is a wrapper for socket.socket() that will return a socket
     inside the namespace specified by the nspath argument, which should be
     a filesystem path to an appropriate namespace file.  You can use the
     get_ns_path() function to generate an appropriate filesystem path if
     you know a namespace name or pid.
-    '''
+    """
 
     with NetNS(nspath=nspath):
         return socket_module.socket(*args)
 
 
 def get_ns_path(nspath=None, nsname=None, nspid=None):
-    '''Generate a filesystem path from a namespace name or pid.
+    """Generate a filesystem path from a namespace name or pid.
 
     Generate a filesystem path from a namespace name or pid, and return
     a filesystem path to the appropriate file.  Returns the nspath argument
     if both nsname and nspid are None.
-    '''
+    """
 
     if nsname:
-        nspath = '/var/run/netns/%s' % nsname
+        nspath = "/var/run/netns/%s" % nsname
     elif nspid:
-        nspath = '/proc/%d/ns/net' % nspid
+        nspath = "/proc/%d/ns/net" % nspid
 
     if not os.path.exists(nspath):
-        raise ValueError('namespace path %s does not exist' % nspath)
+        raise ValueError("namespace path %s does not exist" % nspath)
 
     return nspath
 
 
-class NetNS (object):
-    '''A context manager for running code inside a network namespace.
+class NetNS(object):
+    """A context manager for running code inside a network namespace.
 
     This is a context manager that on enter assigns the current process
     to an alternate network namespace (specified by name, filesystem path,
     or pid) and then re-assigns the process to its original network
     namespace on exit.
-    '''
+    """
 
     def __init__(self, nsname=None, nspath=None, nspid=None):
         self.mypath = get_ns_path(nspid=os.getpid())
-        self.targetpath = get_ns_path(nspath,
-                                      nsname=nsname,
-                                      nspid=nspid)
+        self.targetpath = get_ns_path(nspath, nsname=nsname, nspid=nspid)
 
         if not self.targetpath:
-            raise ValueError('invalid namespace')
+            raise ValueError("invalid namespace")
 
     def __enter__(self):
         # before entering a new namespace, we open a file descriptor
